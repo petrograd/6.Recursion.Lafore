2828132d987954c4362200ca50d5641a67b3a09e
Petr <pmerkuryev@gmail.com>
2025-06-29 18:23:49 +0200

    StackTriangleApp implemented

--- src/StackTriangleApp.java
+++ src/StackTriangleApp.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Params {
     public int n;
     public int returnAddress;
@@ -27,4 +29,66 @@ class StackX {
     }
 }
 public class StackTriangleApp {
+    static int theNumber;
+    static int theAnswer;
+    static StackX theStack;
+    static int codePart;
+    static Params theseParams;
+
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        theNumber = getInt();
+        recTriangle();
+        System.out.println("Triangle=" + theAnswer);
+    }
+
+    private static void recTriangle() {
+        theStack = new StackX(10000);
+        codePart = 1;
+        while (step() == false){
+
+        }
+
+    }
+
+    private static boolean step() {
+        switch(codePart) {
+            case 1:
+                theseParams = new Params(theNumber, 6);
+                theStack.push(theseParams);
+                codePart = 2;
+                break;
+            case 2:
+                theseParams = theStack.peek();
+                if (theseParams.n == 1) {
+                    theAnswer = 1;
+                    codePart = 5;
+                } else
+                    codePart = 3;
+                break;
+            case 3:
+                Params newParams = new Params(theseParams.n - 1, 4);
+                theStack.push(newParams);
+                codePart = 2;
+                break;
+            case 4:
+                theseParams = theStack.peek();
+                theAnswer = theAnswer + theseParams.n;
+                codePart = 5;
+                break;
+            case 5:
+                theseParams = theStack.peek();
+                codePart = theseParams.returnAddress; // 4 or 6
+                theStack.pop();
+                break;
+            case 6:
+                return true;
+        }
+        return false;
+    }
+
+    private static int getInt() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
 }
